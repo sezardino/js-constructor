9e68308d4265ae3541bb1d55724082b881c7adcc
Edward <e.arechwa@gmail.com>
2020-12-31 16:46:55 +0100

    add class suport for usability

--- src/model.js
+++ src/model.js
@@ -1,45 +1,37 @@
+import Block from './scripts/blocks/block';
+
 const model = [
-  {
-    type: 'title',
-    value: 'JavaScript constructor',
-    options: {
-      tag: 'h2',
-      styles: {
-        background:
-          'linear-gradient(90deg, #ffffff 0%, #ff1b00 50%, #ffffff 100%)',
-        color: '#001061',
-        padding: '1.5rem',
-        'text-align': 'center',
-      },
+  new Block('title', 'JavaScript constructor', {
+    tag: 'h2',
+    styles: {
+      background:
+        'linear-gradient(90deg, #ffffff 0%, #ff1b00 50%, #ffffff 100%)',
+      color: '#001061',
+      padding: '1.5rem',
+      'text-align': 'center',
     },
-  },
-  {
-    type: 'text',
-    value:
-      'JavaScript Constructor created on pure JavaScript, without any frameworks and libraries',
-    options: {
+  }),
+  new Block(
+    'text',
+    'JavaScript Constructor created on pure JavaScript, without any frameworks and libraries',
+    {
       styles: {
         'text-align': 'center',
         padding: '1.5rem',
         'font-size': '18px',
       },
+    }
+  ),
+  new Block('columns', ['Content', 'for', 'columns'], {
+    styles: {
+      'text-align': 'center',
+      padding: '1.5rem',
     },
-  },
-  {
-    type: 'columns',
-    value: ['Content', 'for', 'columns'],
-    options: {
-      styles: {
-        'text-align': 'center',
-        padding: '1.5rem',
-      },
-    },
-  },
-  {
-    type: 'image',
-    value:
-      'https://img5.goodfon.ru/wallpaper/nbig/8/9c/abstraktsiia-fon-kartinka-sinii-goluboi-smeshenie-tsvetov-kr.jpg',
-    options: {
+  }),
+  new Block(
+    'image',
+    'https://img5.goodfon.ru/wallpaper/nbig/8/9c/abstraktsiia-fon-kartinka-sinii-goluboi-smeshenie-tsvetov-kr.jpg',
+    {
       styles: {
         width: '100%',
         display: 'flex',
@@ -50,8 +42,8 @@ const model = [
         width: '75%',
       },
       alt: 'alternative text',
-    },
-  },
+    }
+  ),
 ];
 
 export default model;
---
+++ src/scripts/blocks/block.js
@@ -0,0 +1,9 @@
+class Block {
+  constructor(type, value, options) {
+    this.type = type;
+    this.value = value;
+    this.options = options;
+  }
+}
+
+export default Block;
